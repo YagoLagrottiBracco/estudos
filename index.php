bca8b62e60c6c5fadf9d56b9a25d5b73d8af1d79
Yago Lagrotti Bracco <yago.lagrotti@gmail.com>
2018-10-18 20:23:01 -0300

    Finalização de página pessoal.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-info">
                 <a class="navbar-brand" href="http://localhost/estudos/index.php"><i class="fas fa-book"> Estudos</i></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -18,58 +18,90 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Alura</a>
+                            <a class="nav-link dropdown-toggle text-light" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Alura</a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="http://localhost/estudos/alura/introducao-javascript/index.html">Introdução JavaScript</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/alura/introducao-javascript/index.html">Introdução JavaScript</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="http://localhost/estudos/alura/jquery-intermediario/index.php">JQuery Intermediário</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/alura/jquery-intermediario/index.php">JQuery Intermediário</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="http://localhost/estudos/alura/php-pdo/PDO/index.php">PHP utilizando o PDO</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/alura/php-pdo/PDO/index.php">PHP utilizando o PDO</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Exercícios</a>
+                            <a class="nav-link dropdown-toggle text-light" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Exercícios</a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="http://localhost/estudos/exercicios/treinaweb/intermediario/index.php">TreinaWeb Intermediário (feito por mim)</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/exercicios/treinaweb/intermediario/index.php">TreinaWeb Intermediário (feito por mim)</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="http://localhost/estudos/exercicios/treinaweb/respostas/exercicio1/index.php">Exercício 1</a>
-                                <a class="dropdown-item" href="http://localhost/estudos/exercicios/treinaweb/respostas/exercicio2/index.php">Exercício 2</a>
-                                <a class="dropdown-item" href="http://localhost/estudos/exercicios/treinaweb/respostas/exercicio3/index.php">Exercício 3</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/exercicios/treinaweb/respostas/exercicio1/index.php">Exercício 1</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/exercicios/treinaweb/respostas/exercicio2/index.php">Exercício 2</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/exercicios/treinaweb/respostas/exercicio3/index.php">Exercício 3</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="http://localhost/estudos/alura/php-pdo/PDO/index.php">PHP utilizando o PDO</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/alura/php-pdo/PDO/index.php">PHP utilizando o PDO</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Livro Zend Certified Engineer</a>
+                            <a class="nav-link dropdown-toggle text-light" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Livro Zend Certified Engineer</a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="http://localhost/estudos/livro-certificado-zend/funcoes/index.php">Funções</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/livro-certificado-zend/funcoes/index.php">Funções</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Material de apoio</a>
+                            <a class="nav-link dropdown-toggle text-light" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Material de apoio</a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="http://localhost/estudos/material-de-apoio/index.php">Informações úteis</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/material-de-apoio/index.php">Informações úteis</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Treinamento pessoal</a>
+                            <a class="nav-link dropdown-toggle text-light" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Treinamento pessoal</a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="http://localhost/estudos/treinamento-pessoal/index.php">HOME (projeto pessoal)</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/treinamento-pessoal/index.php">HOME (projeto pessoal)</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">TreinaWeb</a>
+                            <a class="nav-link dropdown-toggle text-light" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">TreinaWeb</a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="http://localhost/estudos/avancado/fundamentos-linguagem/funcoes.php">Avançado</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/intermediario/index.php">Intermediário</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="http://localhost/estudos/intermediario/index.php">PHP utilizando o PDO</a>
+                                <a class="dropdown-item text-muted" href="http://localhost/estudos/avancado/fundamentos-linguagem/funcoes.php">Avançado</a>
                             </div>
                         </li>
                     </ul>
                 </div>
             </nav>
         </header>
-        
+        <main class="bg-light p-2">
+            <div class="container-fluid">
+                <div class="row text-info m-1 p-1">
+                    <div class="col-lg-4 col-md-3 col-sm-12">
+                        <img src="recursos/imagens/fotos/foto-pessoal.jpg" class="w-100 border border-primary rounded-circle">
+                    </div>
+                    <div class="col-lg-8 col-md-9 col-sm-12">
+                        <div class="p-1 m-2">
+                            <div class="border border border-info rounded">
+                                <h2 class="font-weight-bold p-1 m-1 display-4">Yago Lagrotti Bracco</h2>
+                                <h3 class="pl-3 text-black-50">Olá, sejam bem-vindos a <span class="font-weight-bold">HOME</span> do meu projeto de estudo sobre programação <span class="font-weight-bold">WEB</span>, aqui você poderá ter acesso a todos os módulos e todos os estudos que estou aprendendo conforme vou fazendo cursos e aprimorando meu conhecimento em linguagens web, atualmente estou aprendendo sobre <span class="font-weight-bold">PHP, JavaScript, HTML e CSS.</span></h3>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-9 text-dark">
+                                    <h4 class="border border border-info rounded p-1 mt-1">Para saber mais sobre mim, nesta página se encontram todas as minhas redes sociais e todos os contatos, este projeto foi criado com o intuito de apresentar como portfólio todo o meu aprendizado e como ele foi conquistado até o presente momento.</h4>
+                                </div>
+                                <div class="col-md-3">
+                                    <h5 class="border border border-info rounded pl-1 mt-1">
+                                        <a href="https://www.linkedin.com/in/yago-lagrotti-bracco"><i class="fab fa-linkedin"> LinkedIn</i></a><br>
+                                        <a href="https://plus.google.com/u/0/108502270308771726996"><i class="fab fa-google-plus"> Google Plus</i></a><br>
+                                        <a href="https://www.youtube.com/channel/UCUvdcWRHZ2B6l0LKQQxBoCw"><i class="fab fa-youtube"> YouTube</i></a><br>
+                                        <a href="https://github.com/YagoLagrottiBracco"><i class="fab fa-github"> GitHub</i></a><br>
+                                        <a href="https://www.facebook.com/yago.lagrotti"><i class="fab fa-facebook"> Facebook</i></a><br>
+                                        <a href="https://twitter.com/YagoLagrotti"><i class="fab fa-twitter"> Twitter</i></a><br>
+                                        <a href="https://www.instagram.com/yagolagrottibracco/"><i class="fab fa-instagram"> Instagram</i></a>
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
         <footer>
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
