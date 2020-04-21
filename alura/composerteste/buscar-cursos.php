6f28e7be4bea23902065c89737c7d2e0463987d1
Yago Lagrotti Bracco <yago.lagrotti@outlook.com>
2020-04-21 14:32:46 -0300

    teste

--- alura/composerteste/buscar-cursos.php
+++ alura/composerteste/buscar-cursos.php
@@ -8,8 +8,8 @@ use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client(['base_uri' => 'https://www.alura.com.br/']);
 $crawler = new Crawler();
-
 $buscardor = new Buscador($client, $crawler);
+
 $cursos = $buscardor->buscar('cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
